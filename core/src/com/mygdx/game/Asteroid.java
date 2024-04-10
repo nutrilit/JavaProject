7ded4199af1d365f89918c6b1311f7c4b2185aab
codd <102369774+jj0rd@users.noreply.github.com>
2024-04-10 11:20:12 +0200

    Kolizja asteroid

--- core/src/com/mygdx/game/Asteroid.java
+++ core/src/com/mygdx/game/Asteroid.java
@@ -3,12 +3,14 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import java.util.Random;
 public class Asteroid {
     private Vector2 position;
     private Texture texture;
+    public Sprite sprite;
     private float width;
     private float height;
     private float speed = 100; // Prędkość ruchu asteroidy
@@ -19,11 +21,13 @@ public class Asteroid {
 
     public Asteroid(Texture texture, boolean isRightAligned,float initialHeight) {
         this.texture = texture;
+        this.sprite =  new Sprite(texture);
+        sprite.setScale(3);
         width = 50;
         height = 50;
 
         if (isRightAligned) {
-            position = new Vector2(Gdx.graphics.getWidth(), (initialHeight - height) / 2);
+            position = new Vector2(Gdx.graphics.getWidth()+(sprite.getWidth()), (initialHeight - height) / 2);
             speed = -speed;
         } else {
             position = new Vector2(-width, (initialHeight - height) / 2);
@@ -56,9 +60,22 @@ public class Asteroid {
         return random.nextFloat() * (respawnTimeMax - respawnTimeMin) + respawnTimeMin;
     }
     public void draw(SpriteBatch batch) {
-        batch.draw(texture, position.x, position.y, width, height);
+       // batch.draw(texture, position.x, position.y, width, height);
         update(Gdx.graphics.getDeltaTime());
+
+        sprite.setPosition(position.x, position.y);
+        sprite.draw(batch);
     }
+    void CheckCollision(Bullet bullet) {
+        if (bullet.active == true)
+        {
+            if(this.sprite.getBoundingRectangle().overlaps(bullet.sprite.getBoundingRectangle()))
+            {
+                bullet.active = false;
+            }
+        }
+    }
+
 }
 
 
